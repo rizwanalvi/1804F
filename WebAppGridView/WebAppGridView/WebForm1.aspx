b92d6e73b1c84d928e3c40205045ec6af01b57a2
rizwanalvi <32450360+rizwanalvi@users.noreply.github.com>
2019-06-12 20:34:08 +0500

    GridView Handling

--- WebAppGridView/WebAppGridView/WebForm1.aspx
+++ WebAppGridView/WebAppGridView/WebForm1.aspx
@@ -9,7 +9,30 @@
 <body>
     <form id="form1" runat="server">
     <div>
-     <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+     <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowCancelingEdit="OnCancelEdit" OnRowEditing="OnRowEdit" OnRowUpdating="OnUpdate">
+             
+         <Columns>
+             <asp:BoundField DataField="CustomerId" HeaderText="Customer ID" />
+             <asp:TemplateField HeaderText="Customer Name">
+                 <EditItemTemplate>
+                     <asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("Name") %>'></asp:TextBox>
+                 </EditItemTemplate>
+                 <ItemTemplate>
+                     <asp:Label ID="Label1" runat="server" Text='<%# Bind("Name") %>'></asp:Label>
+                 </ItemTemplate>
+             </asp:TemplateField>
+             <asp:TemplateField>
+                 <ItemTemplate>
+                     <asp:LinkButton ID="LinkButton1" runat="server" CommandName="Edit">Edit</asp:LinkButton>
+                 </ItemTemplate>
+                 <EditItemTemplate>
+                     <asp:LinkButton ID="LinkButton2" runat="server" CommandName="Cancel">Cancel</asp:LinkButton>
+                     <asp:LinkButton ID="LinkButton3" runat="server" CommandName="Update">Update</asp:LinkButton>
+
+                 </EditItemTemplate>
+             </asp:TemplateField>
+         </Columns>
+        </asp:GridView>
     </div>
     </form>
    
--- WebAppGridView/WebAppGridView/WebForm1.aspx.cs
+++ WebAppGridView/WebAppGridView/WebForm1.aspx.cs
@@ -9,19 +9,39 @@ namespace WebAppGridView
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        private SqlConnection _sqlconn = null;
-        private SqlCommand _cmd = null;
+       
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                _sqlconn = new SqlConnection(@"Data Source=FACULTY18;Initial Catalog=CUSTOMERINFO;Integrated Security=False;User ID=sa;Password=sa9;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-                if (_sqlconn.State == System.Data.ConnectionState.Closed)
-                    _sqlconn.Open();
-                _cmd = new SqlCommand("SELECT * FROM Customers", _sqlconn);
-                GridView1.DataSource = _cmd.ExecuteReader();
-                GridView1.DataBind();
+            if (!this.IsPostBack) {
+                LoadGridview();
             }
+        }
+
+        public void LoadGridview() {
+
+            SqlConnection _sqlconn = new SqlConnection(@"Data Source=FACULTY18;Initial Catalog=CUSTOMERINFO;Integrated Security=False;User ID=sa;Password=sa9;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            if(_sqlconn.State ==System.Data.ConnectionState.Closed)
+            _sqlconn.Open();
+            SqlCommand _cmd = new SqlCommand("SELECT * FROM CUSTOMERS", _sqlconn);
+            GridView1.DataSource = _cmd.ExecuteReader();
+            GridView1.DataBind();
+        }
+
+        protected void OnRowEdit(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            LoadGridview();
+        }
+
+        protected void OnCancelEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            LoadGridview();
+        }
+
+        protected void OnUpdate(object sender, GridViewUpdateEventArgs e)
+        {
+
         }
     }
 }
